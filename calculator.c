53098a3a94a602062c9c2d73a0bde0c68f7a7c89
Bharat-Sharma-007 <bharatsunil2007@gmail.com>
2025-09-02 11:22:59 +0530

    code added

---
+++ calculator.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a, b;
+    printf("Enter the first number:");
+    scanf("%f", &a);
+    printf("Enter the second number:");
+    scanf("%f", &b);
+    int c;
+    printf("Enter the number next to the operator you want to do\n1 = +\n2 = -\n3 = *\n4 = /\n5 = % \n");
+    scanf("%d", &c);
+    float d;
+    d = fmod(a, b);
+
+    switch (c)
+    {
+    case 1:
+    printf("Your answer is: %.2f", a+b);
+    break;
+    case 2:
+    printf("Your answer is: %.2f", a-b);
+    break;
+    case 3:
+    printf("Your answer is: %.2f", a*b);
+    break;
+    case 4:
+    printf("Your answer is: %.2f", a/b);
+    break;
+    case 5:
+    printf("Your answer is: %.2f", d);
+    break;
+    default:
+    printf("Enter a valid operator number");
+    }
+    return 0;
+}
